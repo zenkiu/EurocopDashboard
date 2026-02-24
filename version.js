f1eec9f6394b1c11a068f6c7b25dc4503b09a1ec
zzenkiu sys. <zzenkiu@gmail.com>
2026-02-24 23:02:02 +0100

    Add files via upload

--- version.js
+++ version.js
@@ -1,4 +1,3 @@
 /* --- CONTROL DE VERSIONES CENTRALIZADO --- */
 /* Cambia este valor para actualizar Cache y UI */
-/* Incorporada la opción de clima*/
-const EUROCOP_VERSION = "3.0";
+const EUROCOP_VERSION = "3.0.1";
