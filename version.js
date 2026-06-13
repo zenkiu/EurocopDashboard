1bdc0f455d56252e043f13b77e6ea341a8abb164
zzenkiu sys. <zzenkiu@gmail.com>
2026-06-13 13:23:53 +0200

    Add files via upload

--- version.js
+++ version.js
@@ -1,10 +1,10 @@
 /* --- CONTROL DE VERSIONES CENTRALIZADO --- */
 /* Cambia este valor para actualizar Cache y UI */
-const EUROCOP_VERSION = "3.10.10";
+const EUROCOP_VERSION = "3.10.11";
 
 /* --- TEXTO DE MEMORIAS (editable) --- */
 const EUROCOP_MEMORIAS_INFO =
-    "📦 v3.10.10 · 13/06/2026\n" +
+    "📦 v3.10.11 · 06/05/2026\n" +
     "1.TABLA HECHOS: Carga de 2 tabla de hechos (motivos).\n" +
     "2.MODO Dia/Noche.\n" +
     "3.MAPA: Mejora con posibilidad Edicion Geometrias.";
