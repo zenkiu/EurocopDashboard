ea633c9c8484169ca5a2dc5e301f73b46740623b
zzenkiu sys. <zzenkiu@gmail.com>
2026-08-21 09:18:27 +0200

    Add files via upload

--- version.js
+++ version.js
@@ -1,8 +1,9 @@
 /* --- CONTROL DE VERSIONES CENTRALIZADO --- */
 /* Cambia este valor para actualizar Cache y UI */
-const EUROCOP_VERSION = "3.10.20";
+const EUROCOP_VERSION = "3.10.29";
 
 /* --- TEXTO DE MEMORIAS (editable) --- */
 const EUROCOP_MEMORIAS_INFO =
-    "📦 v3.10.20 · 02/07/2026\n" +
-    "1.SÍNTESIS: Aplica Clima y filtros.";
+    "📦 v3.10.29 · 21/08/2026\n" +
+    "1.SOPORTE (NUEVO): Añadido botón \"Reportar Incidencia\"\n" +
+    "2.SOPORTE: Actualizada libreria mapbox.";
